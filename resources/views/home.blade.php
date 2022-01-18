33a501b6cbb50c95882b5006faeac6b3dd37198f
Roman Podskalnuk <Olorin8@yandex.ru>
2022-01-18 15:23:35 +0300

    Add and update files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE HTML>
-<html>
- <head>
-  <meta charset="utf-8">
-  <title>Страница приветствия</title>
- </head>
+@extends('main')
+
+@section('title')
+Меню
+@endsection
+
+@section('content')
  <body>
      <p>Страница приветствия будущего проекта</p>
   <p><a href="{{ route('auth')}}">Авторизация</a></p>
   <p><a href="{{ route('categories')}}">Страница новостей</a></p>
   <p><a href="{{ route('add_news')}}">Добавить новость</a></p>
  </body>
-</html>
+ @endsection
+
