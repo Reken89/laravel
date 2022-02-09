45e63e5f1772a333872b308c0ecb627bfa6718a9
Roman Podskalnuk <Olorin8@yandex.ru>
2022-02-09 16:14:11 +0300

    Update files

--- app/Http/Controllers/System/ProfileController.php
+++ app/Http/Controllers/System/ProfileController.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\NewsSaveRequest;
 
+
+
 class ProfileController extends Controller
 {
     public function update(Request $request){
         
+        $user = \Illuminate\Support\Facades\Auth::user();
         
-        return view('update');
+        return view('update', ['user' => $user]);
     }
 }
